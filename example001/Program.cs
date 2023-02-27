70edfd4855bd9441c31c28a19c4fc7fad9b6cf4b
Natali <piratika309@mail.ru>
2023-02-27 23:50:58 +0300

    числа от М до Н

---
+++ example001/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+string Numbers (int M, int N)
+{
+if ( M <= N) return $"{M}" + " " + Numbers (M+1, N);
+else return string.Empty;
+}
+
+Console.WriteLine("Введите число, от которого будет вестись отсчет: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число, до которого будет вестись отсчет: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(Numbers(M,N));
