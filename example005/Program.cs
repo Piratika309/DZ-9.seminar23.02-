9ddcbf9f4f7ad9d17fe4da71df8b60642b873944
Natali <piratika309@mail.ru>
2023-03-02 01:32:17 +0300

    возведение в степень

---
+++ example005/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+int result(int A, int B)
+{
+    if (B == 0) return 1;
+    else return result(A, B-1)* A;
+}
+
+Console.WriteLine("Введите число, которое будет возведено в степень: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите степень числа: ");
+int B = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(result(A, B));
