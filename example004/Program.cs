b60eaac761c93978274a3e7e6ebf143e6ea210da
Natali <piratika309@mail.ru>
2023-03-02 01:02:37 +0300

    функция Аккермана

---
+++ example004/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу вычисления функции Аккермана
+
+int result( int n, int m)
+{
+    if(n == 0) return m + 1;
+    else if ((n > 0) && (m == 0)) return result(n-1,1);
+    else return result(n - 1, result(n, m - 1));
+}
+
+Console.WriteLine("Введите первое число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(result(n,m));
