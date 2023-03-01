e5f8821ed5571978d89b963b65c0a527ec2a1990
Natali <piratika309@mail.ru>
2023-03-02 02:30:42 +0300

    сумма элементов

---
+++ example002/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int Sum (int M, int N)
+{
+if (M == N) return N;
+else return M + Sum(M+1, N);
+}
+Console.WriteLine("Введите число, от которого будет вестись отсчет: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число, до которого будет вестись отсчет: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Sum(M, N));
