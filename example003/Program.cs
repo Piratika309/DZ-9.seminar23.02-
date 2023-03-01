8e3065ac97284f50dec25dfd727fb1263b7659c6
Natali <piratika309@mail.ru>
2023-03-01 23:34:34 +0300

    сумма цифр числа

---
+++ example003/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму цифр числа
+
+int SumNumbers(int number)
+{
+    if (number < 1) return 0;
+    else return SumNumbers(number/10) + number % 10;
+}
+
+Console.Write("Введите число : ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumNumbers(number));
